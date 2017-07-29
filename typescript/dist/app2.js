af8dd32a5b548e75c71feb61b96d4fcc383e6f8c
unknown <bamontelucas@gmail.com>
2017-07-29 15:03:31 -0300

    aula 23

--- typescript/dist/app2.js
+++ typescript/dist/app2.js
@@ -1,63 +1,18 @@
 "use strict";
-var Carro = (function () {
-    function Carro(modelo, numeroDePortas) {
-        this.velocidade = 0;
-        this.modelo = modelo;
-        this.numeroDePortas = numeroDePortas;
-    }
-    Carro.prototype.acelerar = function () {
-        this.velocidade += 10;
-    };
-    Carro.prototype.parar = function () {
-        this.velocidade = 0;
-    };
-    Carro.prototype.velocidadeAtual = function () {
-        return this.velocidade;
-    };
-    return Carro;
-}());
-var Concessionaria = (function () {
-    function Concessionaria(endereco, listaDeCarros) {
-        this.endereco = endereco;
-        this.listaDeCarros = listaDeCarros;
-    }
-    Concessionaria.prototype.fornecerEndereco = function () {
-        return this.endereco;
-    };
-    Concessionaria.prototype.mostrarListaDeCarros = function () {
-        return this.listaDeCarros;
-    };
-    return Concessionaria;
-}());
-var Pessoa = (function () {
-    function Pessoa(nome, carroPreferido) {
-        this.nome = nome;
-        this.carroPreferido = carroPreferido;
-    }
-    Pessoa.prototype.dizerNome = function () {
-        return this.nome;
-    };
-    Pessoa.prototype.dizerCarroPreferido = function () {
-        return this.carroPreferido;
-    };
-    Pessoa.prototype.comprarCarro = function (carro) {
-        this.carro = carro;
-    };
-    Pessoa.prototype.dizerCarroQueTem = function () {
-        return this.carro;
-    };
-    return Pessoa;
-}());
-var carroA = new Carro('Corsa', 2);
-var carroB = new Carro('HB20', 4);
-var carroC = new Carro('Veloster', 3);
+Object.defineProperty(exports, "__esModule", { value: true });
+var Carro_1 = require("./Carro");
+var Concessionaria_1 = require("./Concessionaria");
+var Pessoa_1 = require("./Pessoa");
+var carroA = new Carro_1.default('Corsa', 2);
+var carroB = new Carro_1.default('HB20', 4);
+var carroC = new Carro_1.default('Veloster', 3);
 var carros = [
     carroA,
     carroB,
     carroC
 ];
-var concessionaria = new Concessionaria('Rua Dormevil Forastieri, 2-54', carros);
-var cliente = new Pessoa('Lucas Corrêa', 'Corsa');
+var concessionaria = new Concessionaria_1.default('Rua Dormevil Forastieri, 2-54', carros);
+var cliente = new Pessoa_1.default('Lucas Corrêa', 'Corsa');
 concessionaria.mostrarListaDeCarros().forEach(function (carro) {
     if (carro.modelo === cliente.dizerCarroPreferido()) {
         cliente.comprarCarro(carro);
